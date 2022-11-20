8f37f393cd5b1e80731bcadd28ad8906b881f730
Jahidul Islam <jahidulislam3879@gmail.com>
2022-11-20 21:19:53 +0600

    added

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -1,7 +1,7 @@
 """my_portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from new.views import home 
-from new.views import about 
-from new.views import contact 
+from new.views import home,about,contact,convention,showdata
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("home/", home, name="home"),
-    path("about/", about, name="about"),
-    path("contact/", contact, name="contact"),
+    path('admin/', admin.site.urls),
+    path('',home,name='home'),
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact'),
+    path('convention/',convention,name='convention'),
+    path('data/',showdata,name='showdata'),
 ]
--- new/apps.py
+++ new/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NewConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "new"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'new'
